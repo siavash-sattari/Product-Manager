a864fe4989637d58f941fad49301f1ae93679988
siavash sattari <siavash.sattari.75@gmail.com>
2021-08-16 03:28:20 +0430

    Available Products

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-let products = [];
+let products = [
+  {title: "Book 1", exist: true},
+  {title: "Book 2", exist: false},
+  {title: "Book 3", exist: true},
+  {title: "Book 4", exist: false},
+];
 
 let filters = {
   searchItem: "",
+  availableProducts: false,
 };
 
 let renderProducts = function (products, filters) {
   let filteredProducts = products.filter(function (item) {
     return item.title.toLowerCase().includes(filters.searchItem.toLowerCase());
+  });
+
+  filteredProducts = filteredProducts.filter(function (item) {
+    if (filters.availableProducts) {
+      return item.exist;
+    } else {
+      return true;
+    }
   });
 
   document.querySelector("#products").innerHTML = "";
@@ -24,7 +38,7 @@ document.querySelector("#add-product-form").addEventListener("submit", function 
   e.preventDefault();
   products.push({
     title: e.target.elements.productTitle.value,
-    exists: true,
+    exist: true,
   });
   renderProducts(products, filters);
   e.target.elements.productTitle.value = "";
@@ -32,5 +46,10 @@ document.querySelector("#add-product-form").addEventListener("submit", function 
 
 document.querySelector("#search-products").addEventListener("input", function (e) {
   filters.searchItem = e.target.value;
+  renderProducts(products, filters);
+});
+
+document.querySelector("#available-products").addEventListener("change", function (e) {
+  filters.availableProducts = e.target.checked;
   renderProducts(products, filters);
 });
