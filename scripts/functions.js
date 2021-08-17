5d5e71ac82694b6f5700114dba75a40383828a08
siavash sattari <siavash.sattari.75@gmail.com>
2021-08-17 05:44:32 +0430

    Styling

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,26 +1,20 @@
 // getSaveProducts function :
 
-let getSaveProducts = function () {
+let getSaveProducts = () => {
   let productsJSON = localStorage.getItem("products");
-  if (productsJSON !== null) {
-    return JSON.parse(productsJSON);
-  } else {
-    return [];
-  }
+  return productsJSON !== null ? JSON.parse(productsJSON) : [];
 };
 
 // saveProducts function :
 
-let saveProducts = function (products) {
+let saveProducts = (products) => {
   localStorage.setItem("products", JSON.stringify(products));
 };
 
 // removeProduct function :
 
-let removeProduct = function (id) {
-  let productIndex = products.findIndex(function (item) {
-    return item.id === id;
-  });
+let removeProduct = (id) => {
+  let productIndex = products.findIndex((item) => item.id === id);
   if (productIndex > -1) {
     products.splice(productIndex, 1);
   }
@@ -28,10 +22,8 @@ let removeProduct = function (id) {
 
 // toggleProduct function :
 
-let toggleProduct = function (id) {
-  let product = products.find(function (item) {
-    return item.id === id;
-  });
+let toggleProduct = (id) => {
+  let product = products.find((item) => item.id === id);
   if (product !== undefined) {
     product.exist = !product.exist;
   }
@@ -39,9 +31,9 @@ let toggleProduct = function (id) {
 
 // sortProducts function :
 
-let sortProducts = function (products, sortBy) {
+let sortProducts = (products, sortBy) => {
   if (sortBy === "byEdited") {
-    return products.sort(function (a, b) {
+    return products.sort((a, b) => {
       if (a.updated > b.updated) {
         return -1;
       } else if (a.updated < b.updated) {
@@ -51,7 +43,7 @@ let sortProducts = function (products, sortBy) {
       }
     });
   } else if (sortBy === "byCreated") {
-    return products.sort(function (a, b) {
+    return products.sort((a, b) => {
       if (a.created > b.created) {
         return -1;
       } else if (a.created < b.created) {
@@ -67,12 +59,12 @@ let sortProducts = function (products, sortBy) {
 
 // renderProducts function :
 
-let renderProducts = function (products, filters) {
+let renderProducts = (products, filters) => {
   products = sortProducts(products, filters.sortBy);
-  let filteredProducts = products.filter(function (item) {
+  let filteredProducts = products.filter((item) => {
     return item.title.toLowerCase().includes(filters.searchItem.toLowerCase());
   });
-  filteredProducts = filteredProducts.filter(function (item) {
+  filteredProducts = filteredProducts.filter((item) => {
     if (filters.availableProducts) {
       return item.exist;
     } else {
@@ -82,7 +74,7 @@ let renderProducts = function (products, filters) {
 
   document.querySelector("#products").innerHTML = "";
 
-  filteredProducts.forEach(function (item) {
+  filteredProducts.forEach((item) => {
     document.querySelector("#products").appendChild(createProductDOM(item));
   });
 };
@@ -92,9 +84,10 @@ let renderProducts = function (products, filters) {
 let createProductDOM = function (product) {
   let productEl = document.createElement("div");
   let checkbox = document.createElement("input");
-  let productTitle = document.createElement("a");
+  let productTitle = document.createElement("span");
   let productPrice = document.createElement("span");
   let removeButton = document.createElement("button");
+  let editProduct = document.createElement("a");
 
   checkbox.setAttribute("type", "checkbox");
   checkbox.checked = !product.exist;
@@ -106,13 +99,15 @@ let createProductDOM = function (product) {
   });
 
   productTitle.textContent = product.title;
-  productTitle.setAttribute("href", `/edit-product.html#${product.id}`);
+  productTitle.className = "product-title";
   productEl.appendChild(productTitle);
 
-  productPrice.textContent = product.price;
+  productPrice.textContent = `${product.price} تومان`;
+  productPrice.className = "product-price";
   productEl.appendChild(productPrice);
 
-  removeButton.textContent = "Remove";
+  removeButton.textContent = "حذف";
+  removeButton.className = "remove-button";
   productEl.appendChild(removeButton);
 
   removeButton.addEventListener("click", function () {
@@ -121,9 +116,16 @@ let createProductDOM = function (product) {
     renderProducts(products, filters);
   });
 
+  editProduct.textContent = "ویرایش";
+  editProduct.className = "edit-product";
+  editProduct.setAttribute("href", `/edit-product.html#${product.id}`);
+  productEl.appendChild(editProduct);
+
+  productEl.setAttribute("class", "product-row");
+
   return productEl;
 };
 
 let lastEditMessage = function (timestamp) {
-  return `Last Edit : ${moment(timestamp).locale("fa").fromNow()}`;
+  return `آخرین ویرایش : ${moment(timestamp).locale("fa").fromNow()}`;
 };
