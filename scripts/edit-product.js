08fc48cffc841da6e02dba0a49613a7611058c92
siavash sattari <siavash.sattari.75@gmail.com>
2021-08-17 05:37:03 +0430

    Styling

--- scripts/edit-product.js
+++ scripts/edit-product.js
@@ -4,12 +4,9 @@ let deleteProduct = document.querySelector("#delete-product");
 let lastEdit = document.querySelector("#last-edit");
 
 let productID = location.hash.substring(1);
-// console.log(productID)
 
 let products = getSaveProducts();
-let product = products.find(function (item) {
-  return item.id === productID;
-});
+let product = products.find((item) => item.id === productID);
 
 if (product === undefined) {
   location.assign("/index.html");
@@ -19,35 +16,35 @@ productTitle.value = product.title;
 productPrice.value = product.price;
 lastEdit.textContent = lastEditMessage(product.updated);
 
-productTitle.addEventListener("input", function (e) {
+productTitle.addEventListener("input", (e) => {
   product.title = e.target.value;
   product.updated = moment().valueOf();
   lastEdit.textContent = lastEditMessage(product.updated);
   saveProducts(products);
 });
 
-productPrice.addEventListener("input", function (e) {
+productPrice.addEventListener("input", (e) => {
   product.price = e.target.value;
   product.updated = moment().valueOf();
   lastEdit.textContent = lastEditMessage(product.updated);
   saveProducts(products);
 });
 
-deleteProduct.addEventListener("click", function (e) {
+deleteProduct.addEventListener("click", (e) => {
   removeProduct(product.id);
   saveProducts(products);
   location.assign("/index.html");
 });
 
-window.addEventListener("storage", function (e) {
+window.addEventListener("storage", (e) => {
   if (e.key === "products") {
     products = JSON.parse(e.newValue);
-    product = products.find(function (item) {
-      return item.id === productID;
-    });
+    product = products.find((item) => item.id === productID);
+
     if (product === undefined) {
       location.assign("index.html");
     }
+
     productTitle.value = product.title;
     productPrice.value = product.price;
     lastEdit.textContent = lastEditMessage(product.updated);
